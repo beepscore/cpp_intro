9094f44a331042490d6b04f4135a5bb145f05de5
beepscore <support@beepscore.com>
2015-01-21 21:04:42 -0800

    Add conditionals

--- hello.cpp
+++ hello.cpp
@@ -18,6 +18,19 @@ int main() {
 
     float favNum = 3.141592;
 
+
+    int age = 70;
+    int ageAtLastExam = 16;
+    bool isNotIntoxicated = true;
+
+    if ((age >= 1) && (age < 16)) {
+        cout << "You can't drive" << endl;
+    } else if (!isNotIntoxicated) {
+        cout << "You can't drive" << endl;
+    } else {
+        cout << "You can drive" << endl;
+    }
+
     return 0;
 }
 
